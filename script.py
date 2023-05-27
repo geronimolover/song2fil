c6c2bdc46d79266b66680b0cdcfe72324f3751da
Geronimo Lover <84311253+geronimolover@users.noreply.github.com>
2023-05-27 13:15:43 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,9 +1,7 @@
 StartTxT = """
 Hello {mention}
 
-I'm an advanced songer with many capabilities!
-
-There is no practical limits for my filtering capacity :)
+I'm an advanced music bot with many capabilities!
 
 See help button for commands and more details
 """
@@ -16,31 +14,20 @@ Add me as admin in your group and start filtering :)
 /start - Check if I'm alive!
 /help - Command help
 /about - Something about me!
+/song <code>name</code> - Get songs
 
-<b>Filter Commands;</b>
-
-<code>/add name reply</code>  -  Add filter for name
-<code>/del name</code>  -  Delete filter
-<code>/delall</code>  -  Delete entire filters (Group Owner Only!)
-<code>/viewfilters</code>  -  List all filters in chat
-
-<b>Connection Commands;</b>
-<code>/connect groupid</code>  -  Connect your group to my PM. You can also simply use,
-<code>/connect</code> in groups.
-<code>/connections</code>  -  Manage your connections.
 
 <b>Extras;</b>
-/status  -  Shows current status of your bot (Auth User Only)
+/status  -  Shows current status of your bot (Admin Only)
 /id  -  Shows ID information
 /info userid  -  Shows User Information. Use <code>/info</code> as reply to some message for their details!
 
-© @Mo_Tech_YT
+© @Check_This_Channel
 """
 
 AboutTxT = """
-⭕️ My Name : TroJanz Filter Bot</b>
-⭕️ Developers : @TroJanzHEX 
-⭕️ Upgrader : @Mo_Tech_YT
+⭕️ My Name : The Song Request</b>
+⭕️ Developers : @Geronimo1234 
 ⭕️ Language : Python3
 ⭕️ Library : <a href='https://docs.pyrogram.org/'>Pyrogram 2.0.102</a> 
 """
